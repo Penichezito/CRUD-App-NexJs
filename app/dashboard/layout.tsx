624b04e89dd6a78aa40e7c1676b2547d4f7fd22e
Penichezito <penichebarbosa88@gmail.com>
2025-01-14 10:23:12 -0300

    create and config layout.tsx on root and dashboard folder

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,5 +5,9 @@ export default function Layout({
 }: {
   children: React.ReactNode
 }) {
-  return <DashboardLayout>{children}</DashboardLayout>
+  return (
+    <DashboardLayout>
+        {children}
+    </DashboardLayout>
+    )
 }
