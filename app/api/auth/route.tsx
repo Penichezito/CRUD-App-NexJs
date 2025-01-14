7a7e4202332309d8151e5c5ead616dce88469049
Penichezito <penichebarbosa88@gmail.com>
2025-01-13 23:04:21 -0300

    feat(delete): delete files, change projetct structure

---
+++ app/api/auth/route.tsx
@@ -0,0 +1,38 @@
+// app/api/auth/route.ts
+import { prisma } from '@/lib/prisma'
+import { NextRequest, NextResponse } from 'next/server'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+export async function POST(req: NextRequest) {
+  try {
+    const { email, password } = await req.json()
+    
+    const user = await prisma.user.findUnique({
+      where: { email }
+    })
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return NextResponse.json({ error: 'Credenciais inválidas' }, { status: 401 })
+    }
+
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET || 'secret',
+      { expiresIn: '1d' }
+    )
+
+    const response = NextResponse.json({ token })
+    response.cookies.set('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+    })
+
+    return response
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Erro interno do servidor' },
+      { status: 500 }
+    )
+  }
+}
